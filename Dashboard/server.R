4b4d68ead6d45f10545c4aac85a550718dc18e44
Daniel A <github@dac.ac>
2021-04-23 02:35:30 +0200

    testing

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -31,16 +31,8 @@ TMA <- function(wk, yr, ccaas, age_groups, sexes) {
     }
     
     start_period_pop <- pop %>% dplyr::filter(year == yr & week == wk & sex == sexes & age_group %in% age_groups & ccaa %in% ccaas)
-            start_period_pop <- aggregate(start_period_pop$pop, list(year = start_period_pop$year, week = start_period_pop$week), FUN=sum)
-            selected_wk <- ifelse(wk > 26, wk - 26, wk)
-            pop_num <- (start_period_pop$x * pop_num)[selected_wk]
-        }
-    } else {
-        start_period_pop <- pop %>% dplyr::filter(year == yr & week == wk & sex == sexes & age_group %in% age_groups & ccaa %in% ccaas)
-        start_period_pop <- aggregate(start_period_pop$pop, list(year = start_period_pop$year, week = start_period_pop$week), FUN=sum)
-        pop_num <- start_period_pop$x
-    }
-
+    start_period_pop <- aggregate(start_period_pop$pop, list(year = start_period_pop$year, week = start_period_pop$week), FUN=sum)
+            
     ratio <- death_num / pop_num
     return(ratio)
 }
