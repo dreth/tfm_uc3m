5b170392aec800d71e60d045ee057a010ce59b89
Daniel A <github@dac.ac>
2021-05-01 14:15:52 +0200

    deprecated old version and replace with test serv

--- dashboard/server.R
+++ dashboard/server.R
@@ -21,99 +21,122 @@ death = read.csv('https://raw.githubusercontent.com/dreth/tfm_uc3m/main/data/dea
 years_pop <- unique(pop$year)
 
 # MEASURES AND RATIOS
-# Tasa de mortalidad acumulada
-TMA <- function(wk, yr, ccaas, age_groups, sexes) {
+# Cumulative mortality rate
+CMR <- function(wk, yr, ccaas, age_groups, sexes, cmr_c=FALSE) {
     # initialize number of deaths
     death_num <- 0
-    
+
+    # assuming multiple years+weeks
     # cumulative deaths
     numerator <- death %>% dplyr::filter(year %in% yr & week %in% 1:wk & ccaa %in% ccaas & age %in% age_groups & sex == sexes)
 
-    numerator <- aggregate(numerator$death, list(year = numerator$year, week = numerator$week), FUN=sum)
-    death_num <- sum(numerator$x)
-
+    if (length(wk) > 1 | length(yr) > 1) {
+        # multiple years+weeks
+        numerator <- aggregate(numerator$death, list(year = numerator$year), FUN=sum)
+        death_num <- numerator$x
+    } else {
+        # individual years+weeks
+        numerator <- aggregate(numerator$death, list(year = numerator$year, week = numerator$week), FUN=sum)
+        death_num <- sum(numerator$x)
+    }    
+    
     # pop for week wk
     period_pop <- pop %>% dplyr::filter(year %in% yr & week == wk & sex == sexes & age_group %in% age_groups & ccaa %in% ccaas)
 
-    period_pop <- aggregate(period_pop$pop, list(year = period_pop$year, week = period_pop$week), FUN=sum)
-    ratio <- death_num / sum(period_pop$x)
+    # assuming multiple years+weeks
+    if (length(wk) > 1 | length(yr) > 1) {
+        period_pop <- aggregate(period_pop$pop, list(year = period_pop$year), FUN=sum)
+        if (cmr_c==TRUE) {
+            ratio <- mean(death_num / period_pop$x)
+        } else {
+            ratio <- death_num / period_pop$x
+        }
+    } else {
+    # individual years+weeks
+        period_pop <- aggregate(period_pop$pop, list(year = period_pop$year, week = period_pop$week), FUN=sum)
+        ratio <- death_num / sum(period_pop$x)
+    }
     
     return(ratio)
 } 
 
-TMA_C <- function(ccaas, age_groups, sexes, all=FALSE, sel_week=FALSE) {
-    yrs <- 2010:2019
+# Cumulative mortality rate for a group of years (weekly)
+CMR_C <- function(ccaas, age_groups, sexes, all=FALSE, sel_week=FALSE, yrs=2010:2019) {
     if (all == TRUE) {
         all_weeks <- list()
         for (i in 1:52) {
-            all_weeks[[i]] <- mean(sapply(yrs, function(y) TMA(wk=i, yr=y, ccaas=ccaas, age_groups=age_groups, sexes=sexes)))
+            all_weeks[[i]] <- CMR(wk=i, yr=yrs, ccaas=ccaas, age_groups=age_groups, sexes=sexes, cmr_c=TRUE)
         }
         return(all_weeks)
     } else {
-        med_tma_wk <- mean(sapply(yrs, function(y) TMA(wk=sel_week, yr=y, ccaas=ccaas, age_groups=age_groups, sexes=sexes)))
-        last_tma_wk <- mean(sapply(yrs, function(y) TMA(wk=52, yr=y, ccaas=ccaas, age_groups=age_groups, sexes=sexes)))
-        return(c(med_tma_wk, last_tma_wk))
+        med_cmr_wk <- mean(sapply(yrs, function(y) CMR(wk=sel_week, yr=y, ccaas=ccaas, age_groups=age_groups, sexes=sexes)))
+        last_cmr_wk <- mean(sapply(yrs, function(y) CMR(wk=52, yr=y, ccaas=ccaas, age_groups=age_groups, sexes=sexes)))
+        return(c(med_cmr_wk, last_cmr_wk))
     }
 }
 
-# Tasa de mortalidad relativa acumulada
-TMRA <- function(wk, yr, ccaas, age_groups, sexes, tma_c=FALSE) {    
-    if (length(tma_c) > 1) {
-        wks <- c()
-        yrs <- c()
-        tmras <- c()
-        for (i in yr) {
-            for (j in wk) {
-                yrs <- c(yrs, i)
-                wks <- c(wks, j)
-                tmras <- c(tmras, (TMA(wk=j, yr=i, ccaas=ccaas, age_groups=age_groups, sexes=sexes)-tma_c[[j]])/tma_c[[52]])
-            }
+# Cumulative relative mortality rate
+CRMR <- function(wk, yr, ccaas, age_groups, sexes, all=FALSE, cmr_c_yrs=2010:2019) {    
+    cmr <- CMR(wk=wk, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
+    cmr_c <- CMR_C(sel_week=wk, all=FALSE, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
+    return((cmr - cmr_c[1])/cmr_c[2])
+}
+
+# Betterness factor (cumulative)
+BF <- function(wk, yr, ccaas, age_groups, sexes) {
+    cmr_1 <- CMR(wk=wk, yr=yr-1, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
+    cmr <- CMR(wk=wk, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
+    end_cmr <- CMR(wk=52, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
+    return((cmr_1-cmr)/end_cmr)
+}
+
+# PLOTTING FUNCTIONS
+# historical cmr, crmr and bf
+factors_df <- function(wk, yr, ccaas, age_groups, sexes, type='crmr', cmr_c_yrs=2010:2019) {
+    # Initializing vectors for the df
+    wks <- c()
+    yrs <- c()
+    metric <- c()
+
+    # Loop for cumulative relative mortality rate
+    if (type == 'crmr') {
+        cmr_c <- CMR_C(ccaas=ccaas, age_groups=age_groups, sexes=sexes, all=TRUE, sel_week=FALSE, yrs=cmr_c_yrs)
+        for (j in wk) {
+            yrs <- c(yrs, yr)
+            wks <- c(wks, rep(j,length(yr)))
+            metric <- c(metric, (CMR(wk=j, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)-cmr_c[[j]])/cmr_c[[52]])
         }
-        tmra_df <- data.frame(week=wks, year=yrs, tmra=tmras)
-        tmra_df$year <- as.factor(tmra_df$year)
-        return(tmra_df)
-            
-    } else {
-        tma <- TMA(wk=wk, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
-        tma_c <- TMA_C(sel_week=wk, all=FALSE, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
-        return((tma - tma_c[1])/tma_c[2])
+        result <- data.frame(week=wks, year=yrs, crmr=metric)
+    
+    # Loop for betterness factor
+    } else if (type == 'bf') {
+        for (j in wk) {
+            yrs <- c(yrs, yr)
+            wks <- c(wks, rep(j,length(yr)))
+            metric <- c(metric, BF(wk=j, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes))
+        }
+        result <- data.frame(week=wks, year=yrs, bf=metric)
+    
+    # Loop for cumulative mortality rate
+    } else if (type == 'cmr') {
+        for (j in wk) {
+            yrs <- c(yrs, yr)
+            wks <- c(wks, rep(j,length(yr)))
+            metric <- c(metric, CMR(wk=j, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes))
+        }
+        result <- data.frame(week=wks, year=yrs, cmr=metric)
     }
+    
+    # returning the dataframe after converting the years to factor (for plots)
+    result$year <- as.factor(result$year)
+    return(result)
 }
 
-# Factor de mejora acumulado
-FMA <- function(wk, yr, ccaas, age_groups, sexes) {
-    tma_1 <- TMA(wk=wk, yr=yr-1, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
-    tma <- TMA(wk=wk, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
-    end_tma <- TMA(wk=52, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)
-    return((tma_1-tma)/end_tma)
-}
+#
+df <- factors_df(wk=1:52, yr=2014:2020, ccaas=CCAA, age_groups=AGE_GROUPS, sexes='T', type='cmr')
+plt <- ggplot(data=df %>% dplyr::filter(year %in% 2015:2020), aes_string(x='week', y='cmr')) + geom_line(aes(colour=year))
+plt
 
-wks <- c()
-yrs <- c()
-tmras <- c()
-for (i in 2011:2020) {
-    for (j in 1:52) {
-        yrs <- c(yrs, i)
-        wks <- c(wks, j)
-        tmras <- c(tmras, FMA(wk=j, yr=i, ccaas=CCAA, age_groups=AGE_GROUPS, sexes='T'))
-    }
-}
-tmra_df <- data.frame(week=wks, year=yrs, tmra=tmras)
-tmra_df$year <- as.factor(tmra_df$year)
-plt <- ggplot(data=tmra_df %>% dplyr::filter(year %in% 2015:2020), aes(x=week, y=tmra)) + geom_line(aes(colour=year))
-ggsave('plt.png', plt)
-
-
-tma_c <- TMA_C(CCAA, AGE_GROUPS, 'T', all=TRUE, sel_week=FALSE)
-tmras <- TMRA(wk=1:52, yr=2010:2020, ccaas=CCAA, age_groups=AGE_GROUPS, sexes='T', tma_c=tma_c)
-plt <- ggplot(data=tmras %>% dplyr::filter(year %in% 2015:2020), aes(x=week, y=tmra)) + geom_line(aes(colour=year))
-ggsave('plt.png', plt)
-
-tmras$year <- as.factor(tmras$year)
-ggplot(data=tmras %>% dplyr::filter(year %in% 2015:2019), aes(x=week, y=tmra)) + geom_line(aes(colour=year))
-
-
-project_pop(dataset=pop, yr=2020, initial_week=26, ccaas=c('ES11','ES53'), age_groups='Y80-84', sexes='M')
 
 # SERVER
 shinyServer(
--- deprecated/Dashboard/server_1.R
+++ deprecated/Dashboard/server_1.R
@@ -22,52 +22,31 @@ years_pop <- unique(pop$year)
 
 # MEASURES AND RATIOS
 # Tasa de mortalidad acumulada
-TMA <- function(wk, yr, ccaas, age_groups, sexes, tma_c=FALSE) {
+TMA <- function(wk, yr, ccaas, age_groups, sexes) {
     # initialize number of deaths
     death_num <- 0
-
-    # assuming multiple years+weeks
+    
     # cumulative deaths
     numerator <- death %>% dplyr::filter(year %in% yr & week %in% 1:wk & ccaa %in% ccaas & age %in% age_groups & sex == sexes)
 
-    if (length(wk) > 1 | length(yr) > 1) {
-    # multiple years+weeks
-    numerator <- aggregate(numerator$death, list(year = numerator$year), FUN=sum)
-    death_num <- numerator$x
-    } else {
-    
-
-    # individual years+weeks
     numerator <- aggregate(numerator$death, list(year = numerator$year, week = numerator$week), FUN=sum)
     death_num <- sum(numerator$x)
-    }    
-    
 
     # pop for week wk
     period_pop <- pop %>% dplyr::filter(year %in% yr & week == wk & sex == sexes & age_group %in% age_groups & ccaa %in% ccaas)
 
-    # assuming multiple years+weeks
-    if (length(wk) > 1 | length(yr) > 1) {
-        period_pop <- aggregate(period_pop$pop, list(year = period_pop$year), FUN=sum)
-        if (tma_c==TRUE) {
-            ratio <- mean(death_num / period_pop$x)
-        } else {
-            ratio <- death_num / period_pop$x
-        }
-    } else {
-    # individual years+weeks
-        period_pop <- aggregate(period_pop$pop, list(year = period_pop$year, week = period_pop$week), FUN=sum)
-        ratio <- death_num / sum(period_pop$x)
-    }
+    period_pop <- aggregate(period_pop$pop, list(year = period_pop$year, week = period_pop$week), FUN=sum)
+    ratio <- death_num / sum(period_pop$x)
     
     return(ratio)
 } 
 
-TMA_C <- function(ccaas, age_groups, sexes, all=FALSE, sel_week=FALSE, yrs=2010:2019) {
+TMA_C <- function(ccaas, age_groups, sexes, all=FALSE, sel_week=FALSE) {
+    yrs <- 2010:2019
     if (all == TRUE) {
         all_weeks <- list()
         for (i in 1:52) {
-            all_weeks[[i]] <- TMA(wk=i, yr=yrs, ccaas=ccaas, age_groups=age_groups, sexes=sexes, tma_c=TRUE)
+            all_weeks[[i]] <- mean(sapply(yrs, function(y) TMA(wk=i, yr=y, ccaas=ccaas, age_groups=age_groups, sexes=sexes)))
         }
         return(all_weeks)
     } else {
@@ -78,16 +57,17 @@ TMA_C <- function(ccaas, age_groups, sexes, all=FALSE, sel_week=FALSE, yrs=2010:
 }
 
 # Tasa de mortalidad relativa acumulada
-TMRA <- function(wk, yr, ccaas, age_groups, sexes, all=FALSE, tma_c_yrs=2010:2019) {    
-    if (all == TRUE) {
-        tma_c <- TMA_C(ccaas=ccaas, age_groups=age_groups, sexes=sexes, all=TRUE, sel_week=FALSE, yrs=tma_c_yrs)
+TMRA <- function(wk, yr, ccaas, age_groups, sexes, tma_c=FALSE) {    
+    if (length(tma_c) > 1) {
         wks <- c()
-        yrs <-c()
+        yrs <- c()
         tmras <- c()
-        for (j in wk) {
-            yrs <- c(yrs, yr)
-            wks <- c(wks, rep(j,length(yr)))
-            tmras <- c(tmras, (TMA(wk=j, yr=yr, ccaas=ccaas, age_groups=age_groups, sexes=sexes)-tma_c[[j]])/tma_c[[52]])
+        for (i in yr) {
+            for (j in wk) {
+                yrs <- c(yrs, i)
+                wks <- c(wks, j)
+                tmras <- c(tmras, (TMA(wk=j, yr=i, ccaas=ccaas, age_groups=age_groups, sexes=sexes)-tma_c[[j]])/tma_c[[52]])
+            }
         }
         tmra_df <- data.frame(week=wks, year=yrs, tmra=tmras)
         tmra_df$year <- as.factor(tmra_df$year)
@@ -108,13 +88,32 @@ FMA <- function(wk, yr, ccaas, age_groups, sexes) {
     return((tma_1-tma)/end_tma)
 }
 
-# PLOTTING FUNCTIONS
-# historical tma, tmra and fma
-factors_plot <- function(wk, yr, ccaas, age_groups, sexes, tmra_all=FALSE,)
-tmras <- TMRA(wk=1:52, yr=2010:2020, ccaas=CCAA, age_groups=AGE_GROUPS, sexes='T', all=TRUE)
-plt <- ggplot(data=tmras %>% dplyr::filter(year %in% 2015:2020), aes(x=week, y=tmra)) + geom_line(aes(colour=year))
-plt
+wks <- c()
+yrs <- c()
+tmras <- c()
+for (i in 2011:2020) {
+    for (j in 1:52) {
+        yrs <- c(yrs, i)
+        wks <- c(wks, j)
+        tmras <- c(tmras, FMA(wk=j, yr=i, ccaas=CCAA, age_groups=AGE_GROUPS, sexes='T'))
+    }
+}
+tmra_df <- data.frame(week=wks, year=yrs, tmra=tmras)
+tmra_df$year <- as.factor(tmra_df$year)
+plt <- ggplot(data=tmra_df %>% dplyr::filter(year %in% 2015:2020), aes(x=week, y=tmra)) + geom_line(aes(colour=year))
+ggsave('plt.png', plt)
 
+
+tma_c <- TMA_C(CCAA, AGE_GROUPS, 'T', all=TRUE, sel_week=FALSE)
+tmras <- TMRA(wk=1:52, yr=2010:2020, ccaas=CCAA, age_groups=AGE_GROUPS, sexes='T', tma_c=tma_c)
+plt <- ggplot(data=tmras %>% dplyr::filter(year %in% 2015:2020), aes(x=week, y=tmra)) + geom_line(aes(colour=year))
+ggsave('plt.png', plt)
+
+tmras$year <- as.factor(tmras$year)
+ggplot(data=tmras %>% dplyr::filter(year %in% 2015:2019), aes(x=week, y=tmra)) + geom_line(aes(colour=year))
+
+
+project_pop(dataset=pop, yr=2020, initial_week=26, ccaas=c('ES11','ES53'), age_groups='Y80-84', sexes='M')
 
 # SERVER
 shinyServer(
