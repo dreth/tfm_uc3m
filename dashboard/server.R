86d45fa9a758af19ea1fe0b9df3c7ca0f48a0312
Daniel A <github@dac.ac>
2021-05-26 19:56:53 +0200

    oops reactive

--- dashboard/server.R
+++ dashboard/server.R
@@ -167,7 +167,7 @@ shinyServer(
         # Map data table output
         output$mapDataOutput <- renderTable({
             genChloroplethTable()
-        }, digits=SIG_FIGURES(input$plotMetricMaps))
+        }, digits=SIG_FIGURES(reactive(input$plotMetricMaps)))
         
         # PLOT OUTPUTS
         # Action button to generate mortality plots
