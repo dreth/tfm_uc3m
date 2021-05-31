0e760cfec92f2c3fbfd035767b3697f13470491d
Daniel A <github@dac.ac>
2021-05-31 03:14:44 +0200

    okay?

--- dashboard/global.R
+++ dashboard/global.R
@@ -15,7 +15,7 @@ require(rgdal)
 require(RColorBrewer)
 
 # TRACE 
-options(shiny.trace=TRUE)
+options(shiny.trace=FALSE)
 
 # Running last eurostat update check
 system('bash ./www/scripts/check_eurostat.sh', wait=FALSE)
