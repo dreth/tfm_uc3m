764f9be328d19dcbe98f5b65467608dbb68e1310
Daniel A <github@dac.ac>
2021-06-20 01:24:31 +0200

    test4

--- docker/start.sh
+++ docker/start.sh
@@ -4,4 +4,4 @@ git clone https://github.com/dreth/tfm_uc3m.git
 cd ./tfm_uc3m
 git subtree pull --prefix data https://github.com/dreth/tfm_uc3m_data.git main --squash
 cd ../dashboard
-R -e "shiny::runApp(host = '0.0.0.0', port = 3838, launch.browser=FALSE)"
+R 
