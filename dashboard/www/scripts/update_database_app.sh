9cdd18e4ce41c98064b3a5a73857aba41373be2d
Daniel A <github@dac.ac>
2021-06-25 01:15:40 +0200

    small shell script update

--- dashboard/www/scripts/update_database_app.sh
+++ dashboard/www/scripts/update_database_app.sh
@@ -16,8 +16,6 @@ fi
 cd '../api'
 # python3 './query.py'
 # python3 './dbs_check.py'
-echo "test" >> /tfm_uc3m/data/testfile.txt
 cd '/tfm_uc3m'
-git subtree commit -m "DB update as of $date" --prefix data https://"$GITHUB_USER":"$GITHUB_TOKEN"@github.com/dreth/tfm_uc3m_data.git main
 git subtree push --prefix data https://"$GITHUB_USER":"$GITHUB_TOKEN"@github.com/dreth/tfm_uc3m_data.git main
 echo "Repo update pushed to GitHub"
