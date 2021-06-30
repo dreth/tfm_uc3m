b097258f2df999c53535a9b98a7420f93874f19a
Daniel A <github@dac.ac>
2021-06-30 02:09:40 +0200

    update shell script to update from within the app

--- dashboard/www/scripts/update_database_app.sh
+++ dashboard/www/scripts/update_database_app.sh
@@ -16,10 +16,15 @@ fi
 cd '../api'
 python3 './query.py'
 python3 './dbs_check.py'
-cp -r /tfm_uc3m/data/logs /tfm_uc3m_data/logs
+cp -r /tfm_uc3m/data/logs/update_database.log /tfm_uc3m_data/logs/update_database.log
+cp -r /tfm_uc3m/data/logs/update_history.log /tfm_uc3m_data/logs/update_history.log
 cp -r /tfm_uc3m/data/death.csv /tfm_uc3m_data/death.csv
 cp -r /tfm_uc3m/data/pop.csv /tfm_uc3m_data/pop.csv
 cd /tfm_uc3m_data
+git add ./death.csv
+git add ./pop.csv
+git add ./logs/update_database.log
+git add ./logs/update_history.log
 git commit -m "updated database at: $date"
 git push https://"$GITHUB_USER":"$GITHUB_TOKEN"@github.com/dreth/tfm_uc3m_data.git
 echo "Repo update pushed to GitHub"
