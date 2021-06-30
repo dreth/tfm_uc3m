e6cd19a5e6739a3cb2d698dd7227823e40af72bd
Daniel A <github@dac.ac>
2021-06-30 02:20:01 +0200

    a little better probably

--- dashboard/www/scripts/update_database_app.sh
+++ dashboard/www/scripts/update_database_app.sh
@@ -4,7 +4,7 @@ date=''
 
 # Record the date
 if [ $system == 'Darwin' ] then date=$(date -v-1d +%Y-%m-%d %T)  # MacOS
-elif [ $system == 'Linux' ] then date=$(date +%Y-%m-%d)  # Linux
+elif [ $system == 'Linux' ] then date=$(date "+%Y-%m-%d %T")  # Linux
 else echo "OS not recognized" fi
 
 # Browse to API folder to run python scripts
